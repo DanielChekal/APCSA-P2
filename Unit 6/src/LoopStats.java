2ab64c77c3d9b65fc3aa37b23a96f861cf9992d5
DanielChekal <36010882+DanielChekal@users.noreply.github.com>
2018-02-20 10:19:12 -0800

    Update LoopStats.java

--- Unit 6/src/LoopStats.java
+++ Unit 6/src/LoopStats.java
@@ -1,10 +1,11 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
 //Lab  -
 
 import static java.lang.System.*;
+import java.util.Scanner;
 
 public class LoopStats
 {
@@ -12,28 +13,36 @@ public class LoopStats
 
 	public LoopStats()
 	{
-
+		Scanner test = new Scanner(System.in);
+		System.out.print("First Number :: ");
+		start = test.nextInt();
+		System.out.print("Final Number :: ");
+		stop = test.nextInt();
 
 	}
 
 	public LoopStats(int beg, int end)
 	{
-
+		
 
 	}
 
 	public void setNums(int beg, int end)
 	{
-
+ 
 
 	}
 
 	public int getEvenCount()
 	{
 		int evenCount=0;
-
-
-
+		
+		for (int i = start; i<stop+1; i++)
+		{
+			if (i % 2 == 0){
+				evenCount ++;
+			}
+		}
 
 		return evenCount;
 	}
@@ -42,7 +51,12 @@ public class LoopStats
 	{
 		int oddCount=0;
 
-
+		for (int i = start; i<stop+1; i++)
+		{
+			if (i % 2 == 1){
+				oddCount ++;
+			}
+		}
 
 
 
@@ -53,7 +67,10 @@ public class LoopStats
 	{
 		int total=0;
 
-
+		for (int i = start; i<stop+1; i++)
+		{
+			total = total + i;
+		}
 
 
 		return total;
