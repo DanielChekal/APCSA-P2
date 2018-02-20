c079bab4685c7475d26e88b196ebc6b4cd32f362
DanielChekal <36010882+DanielChekal@users.noreply.github.com>
2018-02-20 10:19:25 -0800

    Update Prime.java

--- Unit 6/src/Prime.java
+++ Unit 6/src/Prime.java
@@ -1,20 +1,25 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
 //Lab  -
 
 import static java.lang.System.*;
+import static java.lang.Math.*;
+
 import java.lang.Math;
+import java.util.Scanner;
 
 public class Prime
 {
 	private int number;
-
+	
 	public Prime()
 	{
-
-
+		Scanner Word = new Scanner(System.in);
+		System.out.print("Give me a number :: ");
+		number = Word.nextInt();
+		
 	}
 
 	public Prime(int num)
@@ -25,32 +30,36 @@ public class Prime
 
 	public void setPrime(int num)
 	{
-
+		
 
 	}
 
 	public boolean isPrime()
 	{
-
-
-
-
-
-
-
-		return true;
+		return false;
 	}
 
 	public String toString()
 	{
-		String output="";
-
-
-
-
-
-
-
+		String output = "";
+		for (int i = 2; i<sqrt(number)+1; i++)
+		{
+			if (number == 2)
+			{
+				output = number + " is prime.";
+				break;
+			}
+			if (number%i == 0)
+			{
+				output = number + " is not prime.";
+				break;
+			}
+			else;
+			{
+				output = number + " is prime.";
+			}
+		}
+		
 		return output;
 	}
 }
