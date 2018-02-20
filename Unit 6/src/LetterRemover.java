3b61dec0a67b94d47b583cad48129dfcf59b46c6
DanielChekal <36010882+DanielChekal@users.noreply.github.com>
2018-02-20 11:11:23 -0800

    Update LetterRemover.java

--- Unit 6/src/LetterRemover.java
+++ Unit 6/src/LetterRemover.java
@@ -1,10 +1,12 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//Â© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
 //Lab  -
 
 import static java.lang.System.*;
+import java.util.Scanner;
+
 
 public class LetterRemover
 {
@@ -13,13 +15,18 @@ public class LetterRemover
 
 	public LetterRemover()
 	{
-		//call set
+		Scanner test = new Scanner(System.in);
+		System.out.print("Give me a sentence :: ");
+		String s = test.nextLine();
+		System.out.print("What letter should I remove? ");
+		char c = test.next().charAt(0);
+	}
+	
+	public LetterRemover(String s, char rem)
+	{
+		setRemover(s,rem);
 	}
 
-	//add in second constructor
-	
-	
-	
 	public void setRemover(String s, char rem)
 	{
 		sentence = s;
@@ -30,15 +37,19 @@ public class LetterRemover
 	{
 		String cleaned=sentence;
 
-
-
-
+		for (int i=0; i<cleaned.length(); i++)
+		{
+			if (cleaned.charAt(i) == lookFor)
+			{
+				cleaned = cleaned.substring(0, i) + cleaned.substring(i,cleaned.length());		
+			}
+		}
 
 		return cleaned;
 	}
 
 	public String toString()
 	{
-		return sentence + " - letter to remove " + lookFor;
+		return sentence + " - letter to remove " + lookFor + "\n" + removeLetters();
 	}
 }
