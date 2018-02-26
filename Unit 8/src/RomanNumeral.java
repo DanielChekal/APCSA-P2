8f498aeb9aaf1fb7588fee3cc89ff01fef6c2759
DanielChekal <Danielchekal@Gmail.com>
2018-02-26 09:36:59 -0800

    Unit 8

--- Unit 8/src/RomanNumeral.java
+++ Unit 8/src/RomanNumeral.java
@@ -1,59 +1,57 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
 //Lab  -
 
-import static java.lang.System.*;
-
-public class RomanNumeral
-{
+public class RomanNumeral {
 	private Integer number;
 	private String roman;
 
-	private final static int[] NUMBERS= {1000,900,500,400,100,90,
-													50,40,10,9,5,4,1};
+	private final static int[] NUMBERS = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
-	private final static String[] LETTERS = {"M","CM","D","CD","C","XC",
-												  "L","XL","X","IX","V","IV","I"};
+	private final static String[] LETTERS = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
-	public RomanNumeral(String str)
-	{
-
-
+	public RomanNumeral(String str) {
+		setRoman(str);
 
 	}
 
-	public RomanNumeral(Integer orig)
-	{
-
-
+	public RomanNumeral(Integer orig) {
+		setNumber(orig);
 
 	}
 
-	public void setNumber(Integer num)
-	{
-
-
-
-
+	public void setNumber(Integer num) {
+		number = num;
 
 	}
 
-	public void setRoman(String rom)
-	{
-
-
+	public void setRoman(String rom) {
+		roman = rom;
 
 	}
 
-	public Integer getNumber()
-	{
-		return number;
+	public Integer getNumber() {
+		int output = 0;
+		for (int i = 0; i < LETTERS.length; i++) {
+			while (roman.indexOf(LETTERS[i]) == 0) {
+				output += NUMBERS[i];
+				roman = roman.substring(LETTERS[i].length());
+			}
+		}
+		return output;
 	}
 
-	public String toString()
-	{
-		return roman + "\n";
+	public String toString() {
+		String output = "";
+		int value = number;
+		for (int i = 0; i < NUMBERS.length; i++) {
+			while (value >= NUMBERS[i]) {
+				output += LETTERS[i];
+				value -= NUMBERS[i];
+			}
+		}
+		return output + "\n";
 	}
 }
