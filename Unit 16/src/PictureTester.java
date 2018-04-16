1516fe748ad24bfc4c54bcd2d55f3996fbfc0f5e
DanielChekal <Danielchekal@Gmail.com>
2018-04-16 10:31:01 -0700

    Unit 16

--- Unit 16/src/PictureTester.java
+++ Unit 16/src/PictureTester.java
@@ -8,21 +8,21 @@
 public class PictureTester {
 	/** Method to test zeroBlue */
 	public static void testZeroBlue() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.zeroBlue();
 		beach.explore();
 	}
 
 	public static void testZeroRed() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.zeroRed();
 		beach.explore();
 	}
 
 	public static void testZeroGreen() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.zeroGreen();
 		beach.explore();
@@ -30,7 +30,7 @@ public class PictureTester {
 
 	/** Method to test mirrorVertical */
 	public static void testMirrorVertical() {
-		Picture caterpillar = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/caterpillar.jpg");
+		Picture caterpillar = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/caterpillar.jpg");
 		caterpillar.explore();
 		caterpillar.mirrorVertical();
 		caterpillar.explore();
@@ -38,7 +38,7 @@ public class PictureTester {
 
 	/** Method to test mirrorTemple */
 	public static void testMirrorTemple() {
-		Picture temple = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/temple.jpg");
+		Picture temple = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/temple.jpg");
 		temple.explore();
 		temple.mirrorTemple();
 		temple.explore();
@@ -46,90 +46,90 @@ public class PictureTester {
 
 	/** Method to test the collage method */
 	public static void testCollage() {
-		Picture canvas = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/640x480.jpg");
+		Picture canvas = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/640x480.jpg");
 		canvas.createCollage();
 		canvas.explore();
 	}
 
 	/** Method to test edgeDetection */
 	public static void testEdgeDetection() {
-		Picture swan = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/swan.jpg");
+		Picture swan = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/swan.jpg");
 		swan.edgeDetection(10);
 		swan.explore();
 	}
 
 	public static void testKeepOnlyBlue() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.keepOnlyBlue();
 		beach.explore();
 	}
 
 	public static void testKeepOnlyRed() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.keepOnlyRed();
 		beach.explore();
 	}
 
 	public static void testKeepOnlyGreen() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.keepOnlyGreen();
 		beach.explore();
 	}
 
 	public static void testNegate() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.negate();
 		beach.explore();
 	}
 
 	public static void testGrayscale() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.grayScale();
 		beach.explore();
 	}
 
 	public static void testFixUnderwater() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/water.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/water.jpg");
 		beach.explore();
 		beach.fixUnderwater();
 		beach.explore();
 	}
 
 	public static void testMirrorArms() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/snowman.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/snowman.jpg");
 		beach.explore();
 		beach.mirrorArms();
 		beach.explore();
 	}
 
 	public static void testMirrorGull() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/seagull.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/seagull.jpg");
 		beach.explore();
 		beach.mirrorGull();
 		beach.explore();
 	}
 
 	public static void testMirrorDiagonal() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.mirrorDiagonal();
 		beach.explore();
 	}
 
 	public static void testCopy() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
-		beach.copy(new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/flower1.jpg"), 0, 0);
+		beach.copy(new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/flower1.jpg"), 0, 0);
 		beach.explore();
 	}
 
 	public static void testBlur() {
-		Picture beach = new Picture("/Users/fireguy/Documents/workspace/Unit 16/src/images/beach.jpg");
+		Picture beach = new Picture("C:/Users/chekald3384/Desktop/APCSA-P2/Unit 16/images/beach.jpg");
 		beach.explore();
 		beach.blur(0, 0, 400, 300);
 		beach.explore();
@@ -144,7 +144,7 @@ public class PictureTester {
 		// uncomment a call here to run a test
 		// and comment out the ones you don't want
 		// to run
-		// testZeroBlue();
+		testZeroBlue();
 		// testKeepOnlyBlue();
 		// testKeepOnlyRed();
 		// testKeepOnlyGreen();
